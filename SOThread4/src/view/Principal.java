f3ee6c1334e3ac8b2785ae281e50c126ab3d96c5
Urameshx <88640471+Urameshx@users.noreply.github.com>
2021-09-09 19:01:00 -0300

    Update Principal.java

--- SOThread4/src/view/Principal.java
+++ SOThread4/src/view/Principal.java
@@ -1,21 +1,21 @@
 package view;
 
-import controller.Thread_controller;
+import controller.ThreadController;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		int dist_max = 100;
 		
-		Thread_controller sapo_1 = new Thread_controller(dist_max);
+		ThreadController sapo_1 = new ThreadController(dist_max);
 			sapo_1.start();
-		Thread_controller sapo_2 = new Thread_controller(dist_max);
+		ThreadController sapo_2 = new ThreadController(dist_max);
 			sapo_2.start();
-		Thread_controller sapo_3 = new Thread_controller(dist_max);
+		ThreadController sapo_3 = new ThreadController(dist_max);
 			sapo_3.start();
-		Thread_controller sapo_4 = new Thread_controller(dist_max);
+		ThreadController sapo_4 = new ThreadController(dist_max);
 			sapo_4.start();
-		Thread_controller sapo_5 = new Thread_controller(dist_max);
+		ThreadController sapo_5 = new ThreadController(dist_max);
 			sapo_5.start();
 	}
 }
